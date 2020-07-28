65a4e13814a9a78fe83be4b51f9a80e0bf891f08
srimul <60817863+srimul@users.noreply.github.com>
2020-07-28 18:06:33 -0500

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -75,26 +75,26 @@ resource "null_resource" "myPublicIps" {
 #count = var.instance_count
 
 provisioner "local-exec" {
-command =  "echo 'masters' > hosts1"
+command =  "echo 'Webserver' > hosts1"
 }
 provisioner "local-exec" {
  command = "echo '${element(module.k8sMaster.k8s_public_ip.*,0)}' >> hosts1"
  }
 provisioner "local-exec" {
-    command=  "echo 'workers' >> hosts1"
+    command=  "echo 'Appserver' >> hosts1"
 }
 provisioner "local-exec" {
      command = "echo '${element(module.k8shost.k8s_public_ip.*,0)}' >> hosts1"
  }
-provisioner "local-exec" {
- command = "echo '${element(module.k8shost.k8s_public_ip.*,1)}' >> hosts1"
- }
+#provisioner "local-exec" {
+# command = "echo '${element(module.k8shost.k8s_public_ip.*,1)}' >> hosts1"
+# }
 provisioner "local-exec" {
  command = "aws ec2 wait instance-status-ok --instance-ids '${element(module.k8shost.k8s_instance_id.*,0)}' --region us-east-2"
  }
-provisioner "local-exec" {
- command = "aws ec2 wait instance-status-ok --instance-ids '${element(module.k8shost.k8s_instance_id.*,1)}'"
- }
+#provisioner "local-exec" {
+# command = "aws ec2 wait instance-status-ok --instance-ids '${element(module.k8shost.k8s_instance_id.*,1)}'"
+# }
 provisioner "local-exec" {
  command = "aws ec2 wait instance-status-ok --instance-ids '${element(module.k8sMaster.k8s_instance_id.*,0)}'"
  }
