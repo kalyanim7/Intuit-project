619fbdc5c4852633facf50309c9d492a9d95bfc2
srimul <prasad.mul@gmail.com>
2020-07-28 20:33:49 -0500

    ansible updated

--- Intuit-Ansible/convert.py
+++ Intuit-Ansible/convert.py
@@ -1,15 +1,15 @@
 #! /usr/bin/python
 
-f1 = open('/opt/ansibleinput/hosts1', "r")
-f2 = open('/opt/ansibleinput/hosts', "w")
+f1 = open('hosts1', "r")
+f2 = open('hosts', "w")
 masters_count = 0
 workers_count = 0
 for line in f1:
-    if (line[0] ==  'm') :
+    if (line[0] ==  'W') :
         masters_count += 1
         line1='['+line.rstrip('\n')+']'+'\n'
         f2.write(line1)
-    elif (line[0] ==  'w') :
+    elif (line[0] ==  'A') :
         masters_count = 0
         workers_count += 1
         line1='['+line.rstrip('\n')+']'+'\n'
